51d6465ef6d9c2c4a1c8af699c70d2610c65fe8f
Vanshika Mishra <74042272+vanshika230@users.noreply.github.com>
2021-09-27 20:27:44 +0530

    Delete OpenCV directory

--- OpenCV/1-readimages.py
+++
@@ -1,5 +0,0 @@
-#reading images using OpenCV
-import cv2 as cv
-img=cv.imread('me.jpg')#reading
-cv.imshow('Me',img)#displaying
-cv.waitKey(0)#waits for infinity time for the user to press a key and exit the window
